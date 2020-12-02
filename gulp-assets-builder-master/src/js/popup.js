684af9dcaeb0e3e6c1fae0d6837ab62a0748c1eb
Nicolas Fradin <n.g.fradin@gmail.com>
2020-12-02 12:03:15 +0100

    add the keyDown (esc) function to the popup

--- gulp-assets-builder-master/src/js/popup.js
+++ gulp-assets-builder-master/src/js/popup.js
@@ -22,6 +22,20 @@ function hideElements(){
     active = false;
 }
 
+// Hide popup when Esc key is typed
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        hideElements();
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     triggers.forEach((item) => {
         item.addEventListener("click", () => {
